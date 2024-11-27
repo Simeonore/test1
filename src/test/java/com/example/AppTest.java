f3219084e0c78c2fcdc342539aa821b199c9899a
Simeonore <t8230155@aueb.gr>
2024-11-27 22:45:29 +0200

    Initial commit with Maven and JUnit test

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void sampleTest() {
+        assertTrue(1 + 1 == 2);
+    }
+}
